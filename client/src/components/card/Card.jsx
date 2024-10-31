7faa75b0b72e3ecea9192a3d8066e33c2c6cd1d5
debangshumukherjee <mukherjeedebangshu2002@gmail.com>
2024-10-31 14:19:41 +0530

    first commit

---
+++ client/src/components/card/Card.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import "./card.scss";
+
+function Card({ item }) {
+  return (
+    <div className="card">
+      <Link to={`/${item.id}`} className="imageContainer">
+        <img src={item.images[0]} alt="" />
+      </Link>
+      <div className="textContainer">
+        <h2 className="title">
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className="address">
+          <img src="/pin.png" alt="" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">Rs. {item.price}</p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src="/bed.png" alt="" />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+            <div className="feature">
+              <img src="/bath.png" alt="" />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+            {/* Status Display */}
+            {item.status && ( 
+                <span className={`status ${item.status ? 'occupied' : ''}`}>
+                  {item.status ? 'Occupied' : 'Not Occupied'}
+                </span>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,45 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+  const { currentUser } = useContext(AuthContext);
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Ghardekho is a real estate website connecting property seekers with
+            owners. Users can explore houses, apartments, and land available for
+            sale or rent, and property owners can easily post new listings. A
+            built-in chat feature allows direct communication for inquiries and
+            negotiations.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
